269e63f123e51da2999af4f7d27e167c7af74dbf
Danielle Heckman <danielle.heckman@duke.edu>
2017-06-18 16:35:20 -0400

    restructuring a bit

---
+++ index.php
@@ -0,0 +1,19 @@
+<head>
+  <meta charset='utf-8'>
+  <meta http-equiv="X-UA-Compatible" content="chrome=1">
+  <meta name="description" content="Dheckman.github.io : ">
+  <link rel="stylesheet" href="css/foundation.css">
+  <link rel="stylesheet" type="text/css" media="screen" href="css/all.css.scss">
+  <script src="https://use.fontawesome.com/d939869cf7.js"></script>
+  <link href='http://fonts.googleapis.com/css?family=Arvo:400,700' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro' rel='stylesheet' type='text/css'>
+  <title>Dheckman.github.io</title>
+</head>
+<body>
+<div class="large-12 columns">
+  <div id="header"><?php include "header.html"; ?></div>
+  <h1 class="title_first_name">danielle heckman</h1>
+  <p>web developer, coffee drinker</p>
+</div>
+</body>
+
